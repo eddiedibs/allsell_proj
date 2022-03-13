51b3d071dd0e414393577f5b2d5d78cd59b30005
eddiedibs <eduFlynn@hotmail.com>
2022-03-12 22:06:12 -0400

    Now user profiles are able to be updated by their users through forms which are handled by views.py located at users_allsell directory

--- users_allsell/views.py
+++ users_allsell/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
-from allsellapp.forms import UserRegistrationForm
+from allsellapp.forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -16,7 +16,7 @@ def registration(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, 'Your account has successfuly been created! Now you can log in!')
+            messages.success(request, 'Your account has successfully been created! Now you can log in!')
             return redirect('login_view')
 
 
@@ -31,4 +31,23 @@ def registration(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users_allsell/profile.html')
+    if request.method == 'POST':
+        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            profile_update_form.save()
+            user_update_form.save()
+            messages.success(request, 'Your account has successfully been updated!')
+            return redirect('profile_view')
+
+
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+
+
+    context = {
+        'user_update_form': user_update_form,
+        'profile_update_form': profile_update_form
+    }
+    return render(request, 'users_allsell/profile.html', context)
