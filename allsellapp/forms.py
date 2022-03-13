d0114bb1e72cc9330c30cd984d6417eb90fc932d
eddiedibs <eduFlynn@hotmail.com>
2022-03-12 21:26:02 -0400

    Profile page info now is able to adjust the content responsively according to the amount of caracters on the div

--- allsellapp/forms.py
+++ allsellapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from users_allsell.models import Profile
 
 
 
@@ -13,5 +14,32 @@ class UserRegistrationForm(UserCreationForm):
         class Meta:
                 #'model' Dictates where to save the form fields above. In this case, it is saved in registration_model db
                 model = User
-                #'fields' specifies the fields that are in Post
+                #'fields' specifies the fields that are in the object
                 fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+
+
+
+
+class UserUpdateForm(forms.ModelForm):
+        email = forms.EmailField()
+        first_name = forms.CharField(max_length=30, required=False)
+        last_name = forms.CharField(max_length=30, required=False)
+
+        class Meta:
+                model = User
+                fields = ['first_name', 'last_name', 'username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+        class Meta:
+                model = Profile
+                fields = ['img']
+
+
+
+
+
+
+
+
+
