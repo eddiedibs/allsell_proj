a51dde49cd99042733dd1781659a9b579e4c5a2f
eddiedibs <eduFlynn@hotmail.com>
2022-05-07 21:31:27 -0400

    Code has been cleaned up a bit, New routes have been added along with new templates

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from products.models import Product_model
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, TemplateView
 from django.views.generic.base import View
 
 class ProdDetailView(DetailView):
     model = Product_model
     template_name = 'product.html'
     context_object_name = 'products'
+
+
+
+class ProdSellView(TemplateView):
+    template_name = 'choosewhattosell.html'
 
 
 
